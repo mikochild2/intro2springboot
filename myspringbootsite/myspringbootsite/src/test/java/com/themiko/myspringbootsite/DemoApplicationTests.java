1e2dab1485931bf6c87f06d08777e0676b817763
mikochild2 <mikochild2@gmail.com>
2020-06-20 16:00:42 -0400

    add springboot setup

---
+++ myspringbootsite/myspringbootsite/src/test/java/com/themiko/myspringbootsite/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.themiko.myspringbootsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
